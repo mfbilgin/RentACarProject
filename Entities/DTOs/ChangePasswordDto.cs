5e30983c809bd3a4cfd6b590dfd420adb495424c
mfbilgin42 <mfbilgin42@users.noreply.github.com>
2021-04-03 13:49:56 +0300

    03.04.2021 13:49 Tüm isterler yerine getirildi

---
+++ Entities/DTOs/ChangePasswordDto.cs
@@ -0,0 +1,7 @@
+namespace Entities.DTOs
+{
+    public class ChangePasswordDto
+    {
+        
+    }
+}
---
+++ WebAPI/Controllers/SaveCardController.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    public class SaveCardController : Controller
+    {
+        // GET
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
